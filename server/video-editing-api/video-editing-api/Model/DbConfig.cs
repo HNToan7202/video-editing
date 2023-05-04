002b6953c5827c66de76bafbcd3fc379acc4f29a
81QuangHuy <ngoquanghuy2210@gmail.com>
2023-05-04 18:02:44 +0700

    upload client

--- server/video-editing-api/video-editing-api/Model/DbConfig.cs
+++ server/video-editing-api/video-editing-api/Model/DbConfig.cs
@@ -1,8 +1,24 @@
-﻿namespace video_editing_api.Model
+﻿using MongoDB.Driver;
+namespace video_editing_api.Model
 {
     public class DbConfig
     {
         public string ConnectionString { get; set; }
         public string DbName { get; set; }
+    }
+    public class MyMongoClient
+    {
+        private readonly IMongoDatabase _database;
+
+        public MyMongoClient(DbConfig config)
+        {
+            var client = new MongoClient(config.ConnectionString);
+            _database = client.GetDatabase(config.DbName);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string collectionName)
+        {
+            return _database.GetCollection<T>(collectionName);
+        }
     }
 }
