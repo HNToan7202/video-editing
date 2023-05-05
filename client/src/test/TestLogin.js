66051aff4dd5f42c8c8ccbcdf32407f89ce42169
CongThanh27 <nguyencongthanh280702@gmail.com>
2023-05-05 18:12:12 +0700

    5/5

--- client/src/test/TestLogin.js
+++ client/src/test/TestLogin.js
@@ -1,21 +1,56 @@
-describe("Sign Up Test Suite", () => {
-  it("should sign up successfully", async () => {
-    await browser.url("http://localhost:3000/signUp");
-    const username = await browser.$("#usernameSignUp");
-    const email = await browser.$("#emailSignUp");
-    const fullname = await browser.$("#fullNameSignUp");
-    const password = await browser.$("#passwordSignUp");
-    const passwordConfirm = await browser.$("#confirmPasswordSignUp");
-    const signUpButton = await browser.$("#signUpButton");
-
-    await username.setValue("QuangHuy01");
-    await email.setValue("ngoquanghuyw22@gmail.com");
-    await fullname.setValue("Ngô Quang Huy");
-    await password.setValue("Huyvff@111");
-    await passwordConfirm.setValue("Huyvff@111");
-    browser.keys("Enter");
-    //await signUpButton.click();
-    expect(browser).toHaveTitle("SignUp");
+describe("Sign in Test Suite", () => {
+  it("Login with wrong username", async () => {
+    //Đăng nhập sai username
+    await browser.url("http://localhost:3000/");
+    const usernameError = await browser.$("#login-username");
+    await usernameError.setValue("congthanh28");
+    const passwordError = await browser.$("#login-password");
+    await passwordError.setValue("28072002Thanh@");
+    const showPassword = await browser.$(".MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeMedium.css-78trlr-MuiButtonBase-root-MuiIconButton-root");
+    await showPassword.click(); 
+    const loginButtonError = await browser.$("#login-button");
+    await loginButtonError.click();   
+    await browser.saveScreenshot("./src/testing/SignIn/wrongusername.png");
+    expect(browser).toHaveTitle("SignIn1");
+  });
+  it("Login with wrong password", async () => {
+    await browser.url("http://localhost:3000");
+    const username = await browser.$("#login-username");
+    await username.setValue("congthanh");    
+    const password = await browser.$("#login-password");
+    await password.setValue("28072002Thanh@1");   
+    const showPassword = await browser.$(".MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeMedium.css-78trlr-MuiButtonBase-root-MuiIconButton-root");
+    await showPassword.click();   
+    const loginButton = await browser.$("#login-button");
+    await loginButton.click();  
+    await browser.saveScreenshot("./src/testing/SignIn/wrongpassword.png");
+    expect(browser).toHaveTitle("SignIn2");
+  });
+  it("Login to the wrong account", async () => {
+    await browser.url("http://localhost:3000");
+    const username = await browser.$("#login-username");
+    await username.setValue("congthanh27"); 
+    const password = await browser.$("#login-password");
+    await password.setValue("28072002Thanh@123"); 
+    const showPassword = await browser.$(".MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeMedium.css-78trlr-MuiButtonBase-root-MuiIconButton-root");
+    await showPassword.click(); 
+    const loginButton = await browser.$("#login-button");
+    await loginButton.click(); 
+    await browser.saveScreenshot("./src/testing/SignIn/wrongaccount.png");
+    expect(browser).toHaveTitle("SignIn3");
+  });
+  it("Logged in successfully", async () => {
+    await browser.url("http://localhost:3000");
+    const username = await browser.$("#login-username");
+    await username.setValue("congthanh"); 
+    const password = await browser.$("#login-password");
+    await password.setValue("28072002Thanh@"); 
+    const showPassword = await browser.$(".MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeMedium.css-78trlr-MuiButtonBase-root-MuiIconButton-root");
+    await showPassword.click();  
+    const loginButton = await browser.$("#login-button");
+    await loginButton.click();
+    await browser.saveScreenshot("./src/testing/SignIn/caseSuccess.png");
+    expect(browser).toHaveTitle("SignIn4");
   });
 });
 
