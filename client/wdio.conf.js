cc2d0d98f2353402aefe8ab18456b054308c6deb
Nguyễn Hoàng Toàn <106101425+HNToan7202@users.noreply.github.com>
2023-05-17 02:35:38 +0700

    Update wdio.conf.js

--- client/wdio.conf.js
+++ client/wdio.conf.js
@@ -17,7 +17,7 @@ export const config = {
       maxInstances: 5,
       //browserName: "chrome",
       acceptInsecureCerts: true,
-      browserName: "MicrosoftEdge",
+      browserName: "firefox",
       // // "ms:edgeOptions": {
       //   args: ["--headless", "--disable-gpu"],
       //   w3c: false,
@@ -38,7 +38,7 @@ export const config = {
   connectionRetryCount: 3,
 
   //services: ["chromedriver"],
-  services: ["edgedriver"],
+  services: ["firefoxdriver"],
   framework: "mocha",
   //
   // The number of times to retry the entire specfile when it fails as a whole
