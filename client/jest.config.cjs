1e27c7958e96572f19ca68278d55dc2f83de0872
Nguyễn Hoàng Toàn <106101425+HNToan7202@users.noreply.github.com>
2023-05-24 23:23:36 +0700

    Update jest.config.cjs

--- client/jest.config.cjs
+++ client/jest.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
     // Thư mục chứa các bộ kiểm tra
     roots: ['./src/test'],
-    testSequencer: './src/test/test-sequencer.cjs',
  
     // Cấu hình Jest sử dụng trình báo cáo HTML để tạo báo cáo
     reporters: [
